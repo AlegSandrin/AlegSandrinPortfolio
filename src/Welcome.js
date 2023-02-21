379379cfd97e8b734d4ee079ee74ba3ed3689266
Alexsandro Urbano <99767577+AlegSandrin@users.noreply.github.com>
2023-02-21 16:42:17 -0300

    fix*

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,37 +2,7 @@ import profile from './img/profile.jpg'
 import { Link, animateScroll as scroll } from "react-scroll";
 
 function Welcome() {
-
-    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    let interval = null;
-
-    document.querySelector("h5").onmouseover = event => {  
-  let iteration = 0;
-  
-  clearInterval(interval);
-  
-  interval = setInterval(() => {
-    event.target.innerHTML = event.target.innerHTML
-      .split("")
-      .map((letter, index) => {
-        if(index < iteration) {
-          return event.target.dataset.value[index];
-        }
-      
-        return letters[Math.floor(Math.random() * 26)]
-      })
-      .join("");
     
-    if(iteration >= event.target.dataset.value.length){ 
-      clearInterval(interval);
-    }
-    
-    iteration += 1 / 3;
-  }, 50 );
-}
-
-  
     return (
          
 <div className="Welcome">
